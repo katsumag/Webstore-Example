b805eb84dfe6c7b43aecba49ceb3280db9a2a3dc
katsumag <goude47@gmail.com>
2020-05-30 10:11:04 +0100

    Initial Commit
    Commit all files

---
+++ js/side-menu.js
@@ -0,0 +1,11 @@
+function openMenu() {
+  document.getElementById("side-menu").style.display="block";
+  document.getElementById("menu-button").style.display="none";
+  document.getElementById("close-button").style.display="block";
+}
+
+function closeMenu() {
+  document.getElementById("side-menu").style.display="none";
+  document.getElementById("menu-button").style.display="block";
+  document.getElementById("close-button").style.display="none";
+}
